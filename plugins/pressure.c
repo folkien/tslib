80f4dbed42ab2045007e1039d60dff552198ab5e
folkien <folkus@gmail.com>
2015-10-26 12:20:37 +0100

    We don't move the data. We treat it like release sample.

--- plugins/pressure.c
+++ plugins/pressure.c
@@ -27,7 +27,6 @@
 struct tslib_pressure {
 	struct tslib_module_info module;
 	unsigned int	threshold;
-//	unsigned int	pmax;
 };
 
 // function return number of samples that are in read buffer. This hungry function could
@@ -37,29 +36,19 @@ pressure_read(struct tslib_module_info *info, struct ts_sample *input_samples, i
 {
 	struct tslib_pressure *p = (struct tslib_pressure *)info;
 	int samples_count;
-	//static int xsave = 0, ysave = 0;
-	//static int press = 0;
 
 	samples_count = info->next->ops->read(info->next, input_samples, nr);
 	if (samples_count >= 0) {
-		int nr = 0, i;
+		int i;
 		struct ts_sample *current_sample;
 
 		for (current_sample = input_samples, i = 0; i < samples_count; i++, current_sample++) {
-            // for pressure lower than threshold we don't accept data i drop it.
+            // for pressure lower than threshold we don't accept data and set it as release.
 			if ((current_sample->pressure != 0) && (current_sample->pressure < p->threshold)) {
-				int left = samples_count - nr - 1;
-				if (left > 0) {
-					memmove(current_sample, current_sample + 1, left * sizeof(struct ts_sample));
-					current_sample--;
-					continue;
-				    break;
-				}
+                current_sample->pressure = 0;
             }
             // for pressure higher than threshold we accept the data
-			nr++;
 		}
-		return nr;
 	}
 	return samples_count;
 }
@@ -93,11 +82,6 @@ static int threshold_vars(struct tslib_module_info *inf, char *str, void *data)
 		p->threshold = v;
 		break;
 
-/*	case 1:
-		p->pmax = v;
-		break;
-*/
-
 	default:
 		return -1;
 	}
@@ -108,7 +92,6 @@ static int threshold_vars(struct tslib_module_info *inf, char *str, void *data)
 static const struct tslib_vars pressure_vars[] =
 {
 	{ "threshold",	(void *)0, threshold_vars }
-//	{ "pmax",	(void *)1, threshold_vars }
 };
 
 #define NR_VARS (sizeof(pressure_vars) / sizeof(pressure_vars[0]))
@@ -125,7 +108,6 @@ TSAPI struct tslib_module_info *pressure_mod_init(struct tsdev *dev, const char 
 	p->module.ops = &pressure_ops;
 
 	p->threshold = 150;
-//	p->pmax = INT_MAX;
 
 	/*
 	 * Parse the parameters.
